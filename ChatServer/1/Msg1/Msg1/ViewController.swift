7fe5f33b8acad90496912c94d01477cccc2baf54
Ravinder Kumar <ravinder_kumar@trekbikes.com>
2019-09-17 18:17:33 -0500

    second commit
    hi

--- ChatServer/1/Msg1/Msg1/ViewController.swift
+++ ChatServer/1/Msg1/Msg1/ViewController.swift
@@ -8,14 +8,35 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+   
+    @IBOutlet weak var tableView: UITableView!
+    
     @IBOutlet weak var messageTextField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    
+        // Register the nibs with table view
+        let nib = UINib.init(nibName: "UserTableViewCell", bundle: nil)
+        self.tableView.register(nib, forCellReuseIdentifier: "UserTableViewCell")
+        
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
     }
 
-
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "UserTableViewCellID", for: indexPath) as? UserTableViewCell
+            return cell!
+    }
+    
 }
 
